9513ef77e5046b36367d42291620f36d662440dd
Berk Can Özel <berkcanozel@gmail.com>
2024-07-13 23:50:05 +0300

    berk-2 gitignore addition

--- target/generated-sources/annotations/com/example/kafkaexample/service/userservice/mapper/UserMapperImpl.java
+++ target/generated-sources/annotations/com/example/kafkaexample/service/userservice/mapper/UserMapperImpl.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2024-07-13T20:08:27+0300",
+    date = "2024-07-13T23:43:49+0300",
     comments = "version: 1.4.2.Final, compiler: javac, environment: Java 20.0.1 (Oracle Corporation)"
 )
 @Component
